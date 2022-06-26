17b3713aff3f647704bab82de9b941f79a2bf5a0
Balaji1014 <balajibsc1014@gmail.com>
2022-06-26 10:52:47 +0530

    Done

---
+++ src/org/test/Test.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Java");
+		
+		System.out.println("Selenium");
+		
+		
+		
+		
+		
+	}
+}
