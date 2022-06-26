103b54f70cfbe3801de6a849faa4ca80485ce655
Balaji1014 <balajibsc1014@gmail.com>
2022-06-26 11:04:15 +0530

    Updated Branch A

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -7,10 +7,10 @@ public class Test {
 		System.out.println("Java");
 		
 		System.out.println("Selenium");
-		
-		
-		
-		
+	
+		String un = "Balaji1014";
+		String pd = "Balaji@1014";
+		String em = "balajibsc1014@gmail.com";
 		
 	}
 }
